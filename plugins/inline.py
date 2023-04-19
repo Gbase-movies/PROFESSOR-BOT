5099d96d44efb64a939acb638552cdc350b49684
Gbase-movies <129382639+Gbase-movies@users.noreply.github.com>
2023-04-19 08:41:29 +0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -33,7 +33,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='Itakupasa umelipia ili nikuruhusu kupata movie na series 😂 ',
+                           switch_pm_text='best you need to subscribe and join our channel 😂 ',
                            switch_pm_parameter="subscribe")
         return
 
