b7c5d9b410f0edf269a392a26d94d3f85a637158
Gbase-movies <129382639+Gbase-movies@users.noreply.github.com>
2023-04-19 06:58:27 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,7 +38,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention, message.from_user.username, temp.U_NAME))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton("➕️ NIWEKE KWENYE GROUP LAKO ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+            InlineKeyboardButton("➕️ ADD ME IN YOUR GROUP ⚡ ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("🔍 SEARCH 🔍", switch_inline_query_current_chat=''), 
             InlineKeyboardButton("💸SUBSCRIBE💸", url="https://m.youtube.com/@SWAFLIX")
@@ -88,7 +88,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton("➕️ NIWEKE KWENYE GROUP LAKO ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+            InlineKeyboardButton("➕️ ADD ME IN YOUR GROUP ⚡ ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("🔍 SEARCH 🔍", switch_inline_query_current_chat=''), 
             InlineKeyboardButton("💸SUBSCRIBE💸", url="https://m.youtube.com/@SWAFLIX")
